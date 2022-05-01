a95420920761d1423bedc12b90a0faf6c57845ce
cicco <codingzx@outlook.com>
2022-05-01 20:53:07 +0800

    init

---
+++ src/views/example/keep-alive/deep-children/DeepChildren.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mt-2">DeepChildren.vue third router render</div>
+  <el-input v-model="testInput" placeholder="input test keepAlive(DeepChildren)" class="widthPx-300" />
+</template>
+
+<script setup name="DeepChildren">
+let testInput = ref('')
+
+onMounted(() => {
+  console.log('DeepChildren')
+})
+</script>
+
+<style scoped lang="scss"></style>
